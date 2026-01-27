17beec3c5c93ba3ba0a35b7e28e76cc9c6edf684
raymondstvictor <kingraymondstvictor@gmail.com>
2026-01-27 18:41:15 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,32 @@
+
 import CloudinaryGallery from '../components/CloudinaryGallery';
 
-
 export default function Home() {
-
   return (
-
     <main style={{ padding: '80px 20px', textAlign: 'center' }}>
-
       <h1>KRANDSON Photography</h1>
-
       <p>Luxury 360 Photo Booth Experiences</p>
-  <h2 style={{ marginTop: '50px' }}>360 Photo Booth</h2>
 
-<CloudinaryGallery folder="360-photo-booth" />
+      <h2 style={{ marginTop: '50px' }}>360 Photo Booth</h2>
+      <CloudinaryGallery folder="360-photo-booth" />
 
-
-
-<h2 style={{ marginTop: '50px' }}>Luxury LED iPad Photo Booth</h2>
-
-<CloudinaryGallery folder="ipad-photo-booth" />
-
-
+      <h2 style={{ marginTop: '50px' }}>Luxury LED iPad Photo Booth</h2>
+      <CloudinaryGallery folder="ipad-photo-booth" />
 
       <a
-
         href="tel:19179074544"
-
         style={{
-
           display: 'inline-block',
-
-          marginTop: '25px',
-
+          marginTop: '40px',
           padding: '15px 30px',
-
-          backgroundColor: 'gold',
-
-          color: 'black',
-
+          background: '#d4af37',
+          color: '#000',
+          borderRadius: '6px',
           fontWeight: 'bold',
-
-          textDecoration: 'none',
-
-          borderRadius: '6px'
-
         }}
-
       >
-
         Call / Text Now
-
       </a>
-
     </main>
-
-  )
-
+  );
 }
-
