ac611dddc10563f8f5c643157536548a5c555e95
raymondstvictor <kingraymondstvictor@gmail.com>
2026-01-27 19:55:56 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,1 @@
 
-import GalleryTest from '../components/GalleryTest';
-
-export default function Home() {
-  return (
-    <main style={{ padding: '80px 20px', textAlign: 'center' }}>
-      <h1>KRANDSON Photography</h1>
-
-      <h2 style={{ marginTop: '50px' }}>360 Photo Booth</h2>
-      <GalleryTest />
-
-      <h2 style={{ marginTop: '50px' }}>Luxury LED iPad Photo Booth</h2>
-      <GalleryTest />
-    </main>
-  );
-}
