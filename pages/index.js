82d5489343eafa39d984b1b3b2615afb00da8afa
raymondstvictor <kingraymondstvictor@gmail.com>
2026-01-28 15:42:06 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,13 +10,13 @@ export default function Home() {
       {/* 360 PHOTO BOOTH */}
       <section style={{ marginTop: '60px' }}>
         <h2>360 Photo Booth</h2>
-        <CloudinaryGallery folder="Home/360-photo-booth" />
+        <CloudinaryGallery folder="360-photo-booth" />
       </section>
 
       {/* IPAD PHOTO BOOTH */}
       <section style={{ marginTop: '60px' }}>
         <h2>iPad Photo Booth</h2>
-        <CloudinaryGallery folder="Home/ipad-photo-booth" />
+        <CloudinaryGallery folder="ipad-photo-booth" />
       </section>
     </main>
   );
