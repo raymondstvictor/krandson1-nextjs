87bb9b2f1b3d70dafb24a519f6a83cc87b44e958
raymondstvictor <kingraymondstvictor@gmail.com>
2026-01-28 18:40:08 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,20 @@
+import CloudinaryGallery from "../components/CloudinaryGallery";
 
-<CloudinaryGallery from />
+export default function Home() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>KRANDSON Photography</h1>
+      <p>Luxury 360 Photo Booth Experiences</p>
 
+      <section style={{ marginTop: "60px" }}>
+        <h2>360 Photo Booth</h2>
+        <CloudinaryGallery />
+      </section>
+
+      <section style={{ marginTop: "60px" }}>
+        <h2>iPad Photo Booth</h2>
+        <CloudinaryGallery />
+      </section>
+    </main>
+  );
+}
