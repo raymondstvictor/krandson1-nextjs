81d85a9de76af1b6a511b70f7527a937b4f23bed
raymondstvictor <kingraymondstvictor@gmail.com>
2026-01-27 21:09:07 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,9 @@
 
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>KRANDSON Photography</h1>
+      <p>If you can see this, the page is working.</p>
+    </main>
+  );
+}
