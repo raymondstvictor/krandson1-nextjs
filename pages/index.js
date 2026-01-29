60c642d2b3ccfaf6192d07682d3fee7b973f1f30
raymondstvictor <kingraymondstvictor@gmail.com>
2026-01-28 19:38:53 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,54 @@
+
 export default function Home() {
   return (
-    <main style={{ padding: "40px" }}>
+    <main style={{ padding: 40 }}>
       <h1>KRANDSON Photography</h1>
       <p>Luxury 360 Photo Booth Experiences</p>
 
-      <section style={{ marginTop: "60px" }}>
+      {/* 360 PHOTO BOOTH */}
+      <section style={{ marginTop: 60 }}>
         <h2>360 Photo Booth</h2>
 
-        <img src="/360-photo-booth/photo1.jpg" style={{ width: "250px", marginRight: "10px" }} />
-        <img src="/360-photo-booth/photo2.jpg" style={{ width: "250px" }} />
+        <img
+          src="/360-photo-booth/photo1.jpg"
+          alt="360 Booth"
+          style={{ width: "300px", marginRight: 20 }}
+        />
 
-        <video src="/360-photo-booth/video1.mp4" controls width="400" />
+        <img
+          src="/360-photo-booth/photo2.jpg"
+          alt="360 Booth"
+          style={{ width: "300px" }}
+        />
+
+        <video
+          src="/360-photo-booth/video1.mp4"
+          controls
+          style={{ width: "300px", display: "block", marginTop: 20 }}
+        />
       </section>
 
-      <section style={{ marginTop: "60px" }}>
+      {/* IPAD PHOTO BOOTH */}
+      <section style={{ marginTop: 60 }}>
         <h2>iPad Photo Booth</h2>
 
-        <img src="/ipad-photo-booth/ipad1.jpg" style={{ width: "250px", marginRight: "10px" }} />
-        <img src="/ipad-photo-booth/ipad2.jpg" style={{ width: "250px" }} />
+        <img
+          src="/ipad-photo-booth/ipad1.jpg"
+          alt="iPad Booth"
+          style={{ width: "300px", marginRight: 20 }}
+        />
 
-        <video src="/ipad-photo-booth/ipad-video.mp4" controls width="400" />
+        <img
+          src="/ipad-photo-booth/ipad2.jpg"
+          alt="iPad Booth"
+          style={{ width: "300px" }}
+        />
+
+        <video
+          src="/ipad-photo-booth/ipad-video.mp4"
+          controls
+          style={{ width: "300px", display: "block", marginTop: 20 }}
+        />
       </section>
     </main>
   );
