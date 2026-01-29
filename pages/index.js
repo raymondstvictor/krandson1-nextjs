987910b64bc92305261dd6f94a4a0bdd6bfd012b
raymondstvictor <kingraymondstvictor@gmail.com>
2026-01-28 19:54:31 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,22 +5,18 @@ export default function Home() {
       <h1>KRANDSON Photography</h1>
       <p>Luxury 360 Photo Booth Experiences</p>
 
-      {/* 360 PHOTO BOOTH */}
       <section style={{ marginTop: 60 }}>
         <h2>360 Photo Booth</h2>
-
         <img
           src="/360-photo-booth/photo1.jpg"
           alt="360 Booth"
           style={{ width: "300px", marginRight: 20 }}
         />
-
         <img
           src="/360-photo-booth/photo2.jpg"
           alt="360 Booth"
           style={{ width: "300px" }}
         />
-
         <video
           src="/360-photo-booth/video1.mp4"
           controls
@@ -28,22 +24,13 @@ export default function Home() {
         />
       </section>
 
-      {/* IPAD PHOTO BOOTH */}
       <section style={{ marginTop: 60 }}>
         <h2>iPad Photo Booth</h2>
-
         <img
           src="/ipad-photo-booth/ipad1.jpg"
           alt="iPad Booth"
           style={{ width: "300px", marginRight: 20 }}
         />
-
-        <img
-          src="/ipad-photo-booth/ipad2.jpg"
-          alt="iPad Booth"
-          style={{ width: "300px" }}
-        />
-
         <video
           src="/ipad-photo-booth/ipad-video.mp4"
           controls
