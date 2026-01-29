ca1b2fba7efe2bdee2f2576aa8ff9ee30013988e
raymondstvictor <kingraymondstvictor@gmail.com>
2026-01-28 19:00:08 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import CloudinaryGallery from "../components/CloudinaryGallery";
-
 export default function Home() {
   return (
     <main style={{ padding: "40px" }}>
@@ -8,12 +6,20 @@ export default function Home() {
 
       <section style={{ marginTop: "60px" }}>
         <h2>360 Photo Booth</h2>
-        <CloudinaryGallery />
+
+        <img src="/360-photo-booth/photo1.jpg" style={{ width: "250px", marginRight: "10px" }} />
+        <img src="/360-photo-booth/photo2.jpg" style={{ width: "250px" }} />
+
+        <video src="/360-photo-booth/video1.mp4" controls width="400" />
       </section>
 
       <section style={{ marginTop: "60px" }}>
         <h2>iPad Photo Booth</h2>
-        <CloudinaryGallery />
+
+        <img src="/ipad-photo-booth/ipad1.jpg" style={{ width: "250px", marginRight: "10px" }} />
+        <img src="/ipad-photo-booth/ipad2.jpg" style={{ width: "250px" }} />
+
+        <video src="/ipad-photo-booth/ipad-video.mp4" controls width="400" />
       </section>
     </main>
   );
